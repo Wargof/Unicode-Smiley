1765f3aba32bb1faeaeee5c506b7f83e6f3db739
Wargof <wargof@hotmail.fr>
2018-06-24 16:35:18 +0200

    Renommage en 'Script-Coding'

--- Script Bash - Génération code HTML contenant les codes Unicodes/presource.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Mise en forme de source.txt
-# Prérequis : 
-#       Tout les espaces remplacés par des retour à la ligne, une chaine de caractère par ligne.
-#       Tout les & supprimés
-
-cat presource.txt | grep "#" > source.txt
